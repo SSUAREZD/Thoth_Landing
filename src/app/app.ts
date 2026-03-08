d9da054d40c11aeddc0a97ed53789540811f30f2
Andres Lamprea <a.lampreal@uniandes.edu.co>
2026-03-08 17:21:36 -0500

    Cambios finales

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Features } from './components/features/features';
+import { Testimonials } from './components/testimonials/testimonials';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Navbar,
+    Hero,
+    Features,
+    Testimonials,
+    Contact,
+    Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Toth_landing_page');
 }
