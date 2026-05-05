35b9a206f899819e210a6cf8069fc318110179d7
Andres Lamprea <a.lampreal@uniandes.edu.co>
2026-05-05 07:57:16 -0500

    Cambios para firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,15 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080',
-  appUrl: 'http://localhost:4201',
+  appUrl: 'https://your-thothapp-deployment.com',
+  firebase: {
+    apiKey: 'AIzaSyCJDA5Qg1dwdBtwq5EoVf5E_XI4xd3U0ak',
+    authDomain: 'thoth-landing.firebaseapp.com',
+    projectId: 'thoth-landing',
+    storageBucket: 'thoth-landing.firebasestorage.app',
+    messagingSenderId: '179223669572',
+    appId: '1:179223669572:web:0dbd89c7d531f6cd014511',
+  },
   getApiUrl: () => {
     if (typeof window !== 'undefined') {
       return `http://${window.location.hostname}:8080`;
